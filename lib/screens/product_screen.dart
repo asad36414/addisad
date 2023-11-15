5a60d69b4e93e4e440951c5626d84f5639b19d76
Asad Ali <asadrind4@gmail.com>
2023-11-15 13:39:25 +0500

    some changes

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class ProductScreen extends StatelessWidget {
+class ProductScreen extends StatefulWidget {
+  @override
+  State<ProductScreen> createState() => _ProductScreenState();
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+  int _selectedImageIndex = 0;
+
+  final List<String> _thumbnailImages = [
+    "assets/LCD.png",
+    "assets/Televisions.png",
+    "assets/LCD.png",
+    "assets/Televisions.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +35,57 @@ class ProductScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Image.asset("assets/LCD.png"),
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Image.asset(
+                      _thumbnailImages[_selectedImageIndex],
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                  ),
+                ),
+                // Thumbnails
+                SizedBox(height: 10),
+                Container(
+                  height: 65,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _thumbnailImages.length,
+                    itemBuilder: (context, index) {
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selectedImageIndex = index;
+                          });
+                        },
+                        child: Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                width: index == _selectedImageIndex ? 2 : 1,
+                                color: index == _selectedImageIndex
+                                    ? Color(
+                                        0xFFFE805F) // Border color for selected thumbnail
+                                    : Colors
+                                        .black26, // No border for unselected thumbnails
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Image.asset(
+                              _thumbnailImages[index],
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -33,7 +95,7 @@ class ProductScreen extends StatelessWidget {
                       children: [
                         SizedBox(height: 30),
                         Text(
-                          "Warm Zipper",
+                          "Black Flat Screen TV",
                           style: TextStyle(
                             color: Colors.black87,
                             fontWeight: FontWeight.w900,
@@ -50,39 +112,100 @@ class ProductScreen extends StatelessWidget {
                         ),
                       ],
                     ),
-                    Text(
-                      "\$300.00",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Color(0xFFFE805F),
-                      ),
+                    Column(
+                      children: [
+                        Text(
+                          "\$300.00",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                            color: Color(0xFFFE805F),
+                          ),
+                        ),
+                        SizedBox(height: 5),
+                        Row(
+                          children: [
+                            Text(
+                              "\$600",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                                color: Colors.black38,
+                                decoration: TextDecoration.lineThrough,
+                                decorationColor: Colors
+                                    .red, // Optional: You can set the color of the strikethrough
+                                decorationThickness:
+                                    2.0, // Optional: You can set the thickness of the strikethrough
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            Text(
+                              "-50%",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black38,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ],
                 ),
                 SizedBox(height: 10),
                 Align(
                   alignment: Alignment.centerLeft,
-                  child: RatingBar.builder(
-                    initialRating: 3,
-                    minRating: 1,
-                    direction: Axis.horizontal,
-                    allowHalfRating: true,
-                    itemCount: 5,
-                    itemSize: 25,
-                    itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
-                    itemBuilder: (context, _) => Icon(
-                      Icons.star,
-                      color: Colors.amber,
-                    ),
-                    onRatingUpdate: (rating) {},
+                  child: Row(
+                    children: [
+                      Text(
+                        "5/4 (36)",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.black38,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      RatingBar.builder(
+                        initialRating: 3,
+                        minRating: 1,
+                        direction: Axis.horizontal,
+                        allowHalfRating: true,
+                        itemCount: 5,
+                        itemSize: 25,
+                        itemPadding: EdgeInsets.symmetric(horizontal: 1.0),
+                        itemBuilder: (context, _) => Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        onRatingUpdate: (rating) {},
+                      ),
+                      Spacer(),
+                      Icon(
+                        Icons.favorite_outline,
+                        color: Color(0xFFFE805F),
+                        size: 30,
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 10),
+                SizedBox(height: 20),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Text(
-                    "Elevate your viewing experience with our sleek black flat screen TV. This cutting-edge television delivers stunning visuals, vibrant colors, and sharp details, making every movie night, gaming session, or TV show binge an absolute delight. Its modern design seamlessly blends into any living space, while advanced technology ensures a captivating and cinematic entertainment experience. Immerse yourself in the world of entertainment with our Black Flat Screen TV – where style meets superior performance",
+                    "Description:",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w900,
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Elevate your viewing experience with our sleek black flat screen TV. This cutting-edge television delivers stunning visuals, vibrant colors, and sharp details, making every movie night, gaming session, or TV show binge an absolute delight. Its modern design seamlessly blends.",
                     style: TextStyle(
                       color: Colors.black54,
                       fontWeight: FontWeight.w400,
@@ -90,47 +213,52 @@ class ProductScreen extends StatelessWidget {
                     ),
                   ),
                 ),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Container(
-                      height: 60,
-                      width: 60,
-                      decoration: BoxDecoration(
-                        color: Color(0x1F989797),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Center(
-                        child: Icon(
-                          Icons.shopping_cart,
-                          color: Color(0xFFFE805F),
-                        ),
-                      ),
-                    ),
-                    // ProductDetailsPopUp(),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: 60,
-                        width: MediaQuery.of(context).size.width / 1.5,
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(15),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 6,
+                              spreadRadius: 2,
+                            ),
+                          ],
+                        ),
+                        child: Icon(
+                          Icons.email,
+                          size: 30,
                           color: Color(0xFFFE805F),
                         ),
-                        child: Center(
-                          child: Text(
-                            "Order Now",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(15),
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 6,
+                              spreadRadius: 2,
                             ),
-                          ),
+                          ],
+                        ),
+                        child: Icon(
+                          Icons.phone,
+                          size: 30,
+                          color: Color(0xFFFE805F),
                         ),
                       ),
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
