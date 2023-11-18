45b4e2bccaff461f1d7688716fbe909601409578
Asad Ali <asadrind4@gmail.com>
2023-11-18 13:37:02 +0500

    fourth change

--- lib/admin/screens/add_post.dart
+++ lib/admin/screens/add_post.dart
@@ -35,116 +35,118 @@ class _AddProductPageState extends State<AddProductPage> {
         title: Text('Add Product'),
         backgroundColor: Color(0xFFFE805F),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: ListView(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  _getImage();
-                },
-                child: Container(
-                  height: 200,
-                  color: Colors.grey,
-                  child: _image == null
-                      ? Center(
-                          child: Icon(
-                            Icons.camera_alt,
-                            size: 50,
-                            color: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _getImage();
+                  },
+                  child: Container(
+                    height: 200,
+                    color: Colors.grey,
+                    child: _image == null
+                        ? Center(
+                            child: Icon(
+                              Icons.camera_alt,
+                              size: 50,
+                              color: Colors.white,
+                            ),
+                          )
+                        : Image.file(
+                            _image!,
+                            height: 200,
+                            fit: BoxFit.cover,
                           ),
-                        )
-                      : Image.file(
-                          _image!,
-                          height: 200,
-                          fit: BoxFit.cover,
-                        ),
-                ),
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  labelText: 'Product Name',
-                  border: OutlineInputBorder(),
-                ),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the product name';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  labelText: 'Product Description',
-                  border: OutlineInputBorder(),
-                ),
-                maxLines: 3,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the product description';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: priceController,
-                decoration: InputDecoration(
-                  labelText: 'Product Price',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.numberWithOptions(decimal: true),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the product price';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 16),
-              TextFormField(
-                controller: quantityController,
-                decoration: InputDecoration(
-                  labelText: 'Product Quantity',
-                  border: OutlineInputBorder(),
-                ),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter the product quantity';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 50),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  height: 60,
-                  width: MediaQuery.of(context).size.width / 1.5,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Color(0xFFFE805F),
                   ),
-                  child: Center(
-                    child: Text(
-                      "Publish Product",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    labelText: 'Product Name',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the product name';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: descriptionController,
+                  decoration: InputDecoration(
+                    labelText: 'Product Description',
+                    border: OutlineInputBorder(),
+                  ),
+                  maxLines: 3,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the product description';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: priceController,
+                  decoration: InputDecoration(
+                    labelText: 'Product Price',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the product price';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: quantityController,
+                  decoration: InputDecoration(
+                    labelText: 'Product Quantity',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the product quantity';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 50),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width / 1.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color(0xFFFE805F),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Publish Product",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
